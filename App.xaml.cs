0dd182fb01003b37dfe5c36877ff41da0c8b4872
tsukumijima <tsukumijima@users.noreply.github.com>
2020-08-11 03:13:26 +0900

    initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,93 @@
+﻿namespace TVRemotePlus_Launcher
+{
+    using System.Windows;
+    using System.Diagnostics;
+    using System.Reflection;
+    using System.IO;
+    using System;
+
+    /// <summary>
+    /// App.xaml の相互作用ロジック
+    /// 参考: https://garafu.blogspot.com/2015/06/dev-tasktray-residentapplication.html
+    /// </summary>
+    public partial class App : Application
+    {
+        /// <summary>
+        /// タスクトレイに表示するアイコン
+        /// </summary>
+        private NotifyIconWrapper notifyIcon;
+        private Process Apache;
+        private string CurrentFolder;
+
+        /// <summary>
+        /// System.Windows.Application.Startup イベント を発生させます。
+        /// </summary>
+        /// <param name="e">イベントデータ を格納している StartupEventArgs</param>
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            this.ShutdownMode = ShutdownMode.OnExplicitShutdown;
+            this.notifyIcon = new NotifyIconWrapper();
+
+            Debug.WriteLine("Event: OnStartup");
+
+            // 現在のフォルダを取得
+            this.CurrentFolder = Directory.GetParent(Assembly.GetExecutingAssembly().Location).FullName;
+
+            // Process オブジェクトを作成
+            this.Apache = new Process();
+            // 実行するファイル
+            this.Apache.StartInfo.FileName = this.CurrentFolder + "\\bin\\Apache\\bin\\httpd.exe";
+            // 作業フォルダ
+            this.Apache.StartInfo.WorkingDirectory = this.CurrentFolder + "\\bin\\Apache\\bin";
+            // コンソールウインドウを開くか
+            this.Apache.StartInfo.CreateNoWindow = true;
+            // シェル機能を使うか
+            this.Apache.StartInfo.UseShellExecute = false;
+            // 出力をストリームに書き込む
+            this.Apache.StartInfo.RedirectStandardOutput = true;
+            this.Apache.StartInfo.RedirectStandardInput = false;
+
+            // イベントハンドラーを登録
+            this.Apache.OutputDataReceived += OnOutputDataReceived;
+
+            // Apache を起動
+            this.Apache.Start();
+
+            //非同期で出力の読み取りを開始
+            this.Apache.BeginOutputReadLine();
+        }
+
+        /// <summary>
+        /// System.Windows.Application.Exit イベント を発生させます。
+        /// </summary>
+        /// <param name="e">イベントデータ を格納している ExitEventArgs</param>
+        protected override void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+            this.notifyIcon.Dispose();
+
+            Debug.WriteLine("Event: OnExit");
+
+			// Apache を（強制的に）終了
+			try
+            {
+                this.Apache.Kill();
+            }
+			catch (InvalidOperationException exception)
+			{
+                Debug.WriteLine("Error: " + exception.Message);
+            }
+        }
+
+        /// <summary>
+        /// System.Diagnostics.Process.OutputDataReceived のイベントハンドラー。 
+        // 行が出力されるたびに呼び出されます。
+        /// </summary>
+        protected void OnOutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            // 出力された文字列を表示する
+            Debug.WriteLine("Apache: " + e.Data);
+        }
+    }
+}
---
+++ NotifyIconWrapper.cs
@@ -0,0 +1,60 @@
+﻿namespace TVRemotePlus_Launcher
+{
+    using System;
+    using System.ComponentModel;
+    using System.Windows;
+
+    /// <summary>
+    /// タスクトレイ通知アイコン
+    /// 参考: https://garafu.blogspot.com/2015/06/dev-tasktray-residentapplication.html
+    /// </summary>
+    public partial class NotifyIconWrapper : Component
+    {
+        /// <summary>
+        /// NotifyIconWrapper クラス を生成、初期化します。
+        /// </summary>
+        public NotifyIconWrapper()
+        {
+            // コンポーネントの初期化
+            this.InitializeComponent();
+
+            // コンテキストメニューのイベントを設定
+            this.toolStripMenuItem_Open.Click += this.toolStripMenuItem_Open_Click;
+            this.toolStripMenuItem_Exit.Click += this.toolStripMenuItem_Exit_Click;
+        }
+
+        /// <summary>
+        /// コンテナ を指定して NotifyIconWrapper クラス を生成、初期化します。
+        /// </summary>
+        /// <param name="container">コンテナ</param>
+        public NotifyIconWrapper(IContainer container)
+        {
+            container.Add(this);
+
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// コンテキストメニュー "サーバー設定…" を選択したとき呼ばれます。
+        /// </summary>
+        /// <param name="sender">呼び出し元オブジェクト</param>
+        /// <param name="e">イベントデータ</param>
+        private void toolStripMenuItem_Open_Click(object sender, EventArgs e)
+        {
+            // MainWindow を生成、表示
+            var window = new MainWindow();
+            window.Show();
+        }
+
+        /// <summary>
+        /// コンテキストメニュー "TVRemotePlus を終了" を選択したとき呼ばれます。
+        /// </summary>
+        /// <param name="sender">呼び出し元オブジェクト</param>
+        /// <param name="e">イベントデータ</param>
+        private void toolStripMenuItem_Exit_Click(object sender, EventArgs e)
+        {
+            // 現在のアプリケーションを終了
+            Application.Current.Shutdown();
+        }
+    }
+}
