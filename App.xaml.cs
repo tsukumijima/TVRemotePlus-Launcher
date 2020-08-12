233b69a6ba10f080f9280563f9c0077c57ab17e7
tsukumijima <tsukumijima@users.noreply.github.com>
2020-08-13 00:53:39 +0900

    動作と外観のブラッシュアップ

--- App.xaml.cs
+++ App.xaml.cs
@@ -5,20 +5,26 @@
     using System.Reflection;
     using System.IO;
     using System;
-	using System.Collections.ObjectModel;
-	using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Text.RegularExpressions;
+    using Microsoft.WindowsAPICodePack.Dialogs;
 
-	/// <summary>
-	/// App.xaml の相互作用ロジック
-	/// 参考: https://garafu.blogspot.com/2015/06/dev-tasktray-residentapplication.html
-	/// </summary>
-	public partial class App : Application
+    /// <summary>
+    /// App.xaml の相互作用ロジック
+    /// 参考: https://garafu.blogspot.com/2015/06/dev-tasktray-residentapplication.html
+    /// </summary>
+    public partial class App : Application
     {
-    
+
         private NotifyIconWrapper notifyIcon;
-        private List<string> Log;
+        private ObservableCollection<string> Log;
         private Process Apache;
         private string CurrentFolder;
+        private string CurrentFilePath;
+        private string CurrentFileName;
+        private string CurrentFileNameWithoutExtension;
 
         /// <summary>
         /// System.Windows.Application.Startup イベント を発生させます。
@@ -26,17 +32,84 @@
         /// <param name="e">イベントデータ を格納している StartupEventArgs</param>
         protected override void OnStartup(StartupEventArgs e)
         {
-            base.OnStartup(e);
-            this.ShutdownMode = ShutdownMode.OnExplicitShutdown;
-            this.notifyIcon = new NotifyIconWrapper();
-
             Debug.WriteLine("Event: OnStartup");
 
-            // ログのコレクションを作成
-            this.Log = new List<string>();
+            // 実行ファイル名を取得
+            this.CurrentFilePath = Assembly.GetExecutingAssembly().Location;
+            this.CurrentFileName = Path.GetFileName(this.CurrentFilePath);
+            this.CurrentFileNameWithoutExtension = Path.GetFileNameWithoutExtension(this.CurrentFilePath);
 
             // 現在のフォルダを取得
-            this.CurrentFolder = Directory.GetParent(Assembly.GetExecutingAssembly().Location).FullName;
+            this.CurrentFolder = Directory.GetParent(this.CurrentFilePath).FullName;
+
+            // ログのコレクションを作成
+            this.Log = new ObservableCollection<string>();
+
+            // ---------- 多重起動を防止 ----------
+
+            // 同じ実行ファイル名のプロセスを取得
+            Process[] processes = Process.GetProcessesByName(this.CurrentFileNameWithoutExtension);
+
+            // 自分自身と同じパスのプロセスを探す
+            var count = 0;
+            foreach (var process in processes)
+			{
+                // 自分自身と同じパスのプロセス
+                if (process.MainModule.FileName == this.CurrentFilePath)
+                {
+                    if (count > 0) // 自分自身はカウントに含めない
+                    {
+                        // エラーダイヤログ
+                        var dialog = new TaskDialog();
+                        dialog.Caption = "エラー";
+                        dialog.InstructionText = "同じサーバー (Apache) を複数起動することはできません";
+                        dialog.Text = "複数の TVRemotePlus を起動したい場合は、フォルダを別にした上で Apache のポートが衝突しないように設定してください。";
+                        dialog.Icon = TaskDialogStandardIcon.Error;
+                        dialog.StandardButtons = TaskDialogStandardButtons.Ok;
+                        dialog.Show();
+
+                        // 現在のアプリケーションを終了
+                        Application.Current.Shutdown();
+                        return;
+                    }
+                    count++;
+				}
+			}
+
+            // ---------- 起動処理 ----------
+
+            base.OnStartup(e);
+            this.ShutdownMode = ShutdownMode.OnExplicitShutdown;
+
+            // ---------- Apache の設定を取得 ----------
+
+            // httpd.conf を開く
+            StreamReader sr = new StreamReader(this.CurrentFolder + "\\bin\\Apache\\conf\\httpd.conf", Encoding.GetEncoding("UTF-8"));
+            string httpd_conf = sr.ReadToEnd();
+
+            // Apache の設定を取得
+            // Apache のローカル IP アドレス
+            Application.Current.Properties["ServerIP"] = Regex.Match(httpd_conf, @"Define SRVIP\s""(?<SRVIP>.*)""").Groups["SRVIP"].Value;
+            // Apache の HTTP ポート
+            Application.Current.Properties["ServerHTTPPort"] = Regex.Match(httpd_conf, @"Define HTTP_PORT\s""(?<HTTP_PORT>.*)""").Groups["HTTP_PORT"].Value;
+            // Apache の HTTPS ポート
+            Application.Current.Properties["ServerHTTPSPort"] = Regex.Match(httpd_conf, @"Define HTTPS_PORT\s""(?<HTTPS_PORT>.*)""").Groups["HTTPS_PORT"].Value;
+            // Apache のサーバールート
+            Application.Current.Properties["ServerRoot"] = Regex.Match(httpd_conf, @"Define SRVROOT\s""(?<SRVROOT>.*)""").Groups["SRVROOT"].Value;
+            // Apache の html ルート (/htdocs 付き)
+            Application.Current.Properties["DocumentRoot"] = Application.Current.Properties["ServerRoot"].ToString().TrimEnd('/') + Regex.Match(httpd_conf, @"DocumentRoot ""\$\{SRVROOT\}(?<FOLDER>.*)""").Groups["FOLDER"].Value;
+
+            Debug.WriteLine("ServerIP: " + Application.Current.Properties["ServerIP"]);
+            Debug.WriteLine("ServerHTTPPort: " + Application.Current.Properties["ServerHTTPPort"]);
+            Debug.WriteLine("ServerHTTPSPort: " + Application.Current.Properties["ServerHTTPSPort"]);
+            Debug.WriteLine("ServerRoot: " + Application.Current.Properties["ServerRoot"]);
+            Debug.WriteLine("DocumentRoot: " + Application.Current.Properties["DocumentRoot"]);
+
+            // タスクトレイにアイコンを表示
+            // Apache の設定が格納されるのを待ってから
+            this.notifyIcon = new NotifyIconWrapper();
+
+            // ---------- Apache を起動 ----------
 
             // Process オブジェクトを作成
             this.Apache = new Process();
@@ -56,16 +129,35 @@
             this.Apache.StartInfo.RedirectStandardError = true;
             this.Apache.StartInfo.RedirectStandardInput = false;
 
+            // プロセスの終了時にイベントを送る
+            this.Apache.EnableRaisingEvents = true;
+            this.Apache.Exited += new EventHandler(OnProcessExited);
+
             // イベントハンドラーを登録
             this.Apache.OutputDataReceived += OnOutputDataReceived;
             this.Apache.ErrorDataReceived += OnErrorDataReceived;
 
-            // Apache を起動
-            this.Apache.Start();
+            try
+            {
+                // Apache を起動
+                this.Apache.Start();
 
-            //非同期で出力の読み取りを開始
-            this.Apache.BeginOutputReadLine();
-            this.Apache.BeginErrorReadLine();
+                //非同期で出力の読み取りを開始
+                this.Apache.BeginOutputReadLine();
+                this.Apache.BeginErrorReadLine();
+            }
+            catch (System.ComponentModel.Win32Exception Exception)
+            {
+                // httpd.exe が存在しないなど、プロセスを開始できない場合
+                Debug.WriteLine("Error: " + Exception.Message);
+                if (Exception.Message != "" && Exception.Message != null) // 空でないなら
+                {
+                    this.Log.Add("サーバー (Apache) を起動できませんでした: " + Exception.Message);
+
+                    // ログを全てのページで見られるように保存
+                    Application.Current.Properties["Log"] = this.Log;
+                }
+            }
         }
 
         /// <summary>
@@ -74,31 +166,34 @@
         /// <param name="e">イベントデータ を格納している ExitEventArgs</param>
         protected override void OnExit(ExitEventArgs e)
         {
-            base.OnExit(e);
-            this.notifyIcon.Dispose();
-
             Debug.WriteLine("Event: OnExit");
 
-			try
+            base.OnExit(e);
+            if (this.notifyIcon != null && this.Apache != null)
             {
-                // Apache を（強制的に）終了
-                this.Apache.Kill();
-            }
-			catch (InvalidOperationException exception)
-			{
-                // Apache がエラーなどで既に終了している場合にスローされる
-                Debug.WriteLine("Error: " + exception.Message);
+                this.notifyIcon.Dispose();
+
+                try
+                {
+                    // Apache を（強制的に）終了
+                    this.Apache.Kill();
+                }
+                catch (InvalidOperationException Exception)
+                {
+                    // Apache がエラーなどで既に終了している場合にスローされる
+                    Debug.WriteLine("Error: " + Exception.Message);
+                }
             }
         }
 
         /// <summary>
-        /// System.Diagnostics.Process.OutputDataReceived のイベントハンドラー。 
+        /// Process.OutputDataReceived のイベントハンドラー。 
         // 行が出力されるたびに呼び出されます。
         /// </summary>
         protected void OnOutputDataReceived(object sender, DataReceivedEventArgs e)
         {
             // 出力された文字列を表示する
-            Debug.WriteLine("Apache: ((" + e.Data + "))");
+            Debug.WriteLine("Apache: " + e.Data + "");
 
             // ログを追加
             if (e.Data != "" && e.Data != null) // 空でないなら
@@ -111,14 +206,14 @@
         }
 
         /// <summary>
-        /// System.Diagnostics.Process.ErrorDataReceived のイベントハンドラー。 
+        /// Process.ErrorDataReceived のイベントハンドラー。 
         // エラー行が出力されるたびに呼び出されます。
         /// </summary>
         protected void OnErrorDataReceived(object sender, DataReceivedEventArgs e)
         {
             // 出力された文字列を表示する
             // 本来はエラーのみ出力されるはずだが Apache の場合はなぜか普通のメッセージも入る
-            Debug.WriteLine("Apache: ((" + e.Data + "))");
+            Debug.WriteLine("Apache: " + e.Data + "");
 
             // ログを追加
             if (e.Data != "" && e.Data != null) // 空でないなら
@@ -128,6 +223,36 @@
 
             // ログを全てのページで見られるように保存
             Application.Current.Properties["Log"] = this.Log;
+        }
+
+        /// <summary>
+        /// Process.Exited のイベントハンドラー。 
+        // プロセスが終了したときに呼び出されます。
+        /// </summary>
+        public void OnProcessExited(object sender, System.EventArgs e)
+        {
+            Console.WriteLine(
+                $"Exit time    : {this.Apache.ExitTime}\n" +
+                $"Exit code    : {this.Apache.ExitCode}\n" +
+                $"Elapsed time : {Math.Round((this.Apache.ExitTime - this.Apache.StartTime).TotalMilliseconds)}");
+
+            var dialog = new TaskDialog();
+            dialog.Caption = "エラー";
+            dialog.InstructionText = "サーバー (Apache) が異常終了しました";
+            dialog.Text = "サーバー (Apache) の設定を開き、ログを確認してください。";
+            dialog.Icon = TaskDialogStandardIcon.Error;
+            dialog.StandardButtons = TaskDialogStandardButtons.Ok;
+            dialog.Show();
+
+            // ログを全てのページで見られるように保存
+            if (this.Log != null && Application.Current != null) // 正常終了時に行わない
+            {
+                this.Log.Add("サーバー (Apache) が異常終了しました: 終了コード: " + this.Apache.ExitTime);
+                this.Log.Add("プロセス開始: " + this.Apache.StartTime + "  プロセス終了: " + this.Apache.ExitTime + "  " +
+                             "経過時間: " + Math.Round((this.Apache.ExitTime - this.Apache.StartTime).TotalMilliseconds) + "ms");
+
+                Application.Current.Properties["Log"] = this.Log;
+            }
         }
     }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,30 +15,57 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MahApps.Metro.Controls;
 
 namespace TVRemotePlus_Launcher
 {
 
-	/// <summary>
-	/// MainWindow.xaml の相互作用ロジック
-	/// </summary>
-	public partial class MainWindow : Window
-	{
-		
-		private List<string> Log;
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        
+        private ObservableCollection<string> Log;
 
-		public MainWindow()
-		{
-			InitializeComponent();
+        public MainWindow()
+        {
+            InitializeComponent();
 
-			// Apache の起動ログを取得
-			this.Log = (List<string>) Application.Current.Properties["Log"];
+            // App.xaml.cs で取得した Apache の起動ログ
+            this.Log = (ObservableCollection<string>) Application.Current.Properties["Log"];
+            this.Log.CollectionChanged += OnLogChanged;
 
-			// ListBox に追加
-			foreach (var Item in this.Log)
-			{
-				ListBox.Items.Add(Item);
-			}
-		}
-	}
+            // ListBox に追加
+            if (this.Log != null) // null でないなら
+            {
+                foreach (var Item in this.Log)
+                {
+                    ListBox.Items.Add(Item);
+                    // ListBox.Items.Add("TestLog TestLog TestLog TestLog");
+                }
+            }
+        }
+
+        /// <summary>
+        /// System.Collections.ObjectModel.ObservableCollection.CollectionChanged のイベントハンドラー。 
+        // ログが変更されたときに呼び出されます。
+        /// </summary>
+        private void OnLogChanged (object sender, NotifyCollectionChangedEventArgs e)
+        {
+
+            // 新しいログを追加
+            var NewLog = e.NewItems;
+
+            // ListBox に追加
+            if (NewLog != null) // null でないなら
+            {
+                foreach (var Item in NewLog)
+                {
+                    Debug.WriteLine(Item);
+                    //ListBox.Items.Add(Item);
+                }
+            }
+        }
+    }
 }
