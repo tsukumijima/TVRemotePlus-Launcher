6d1f225c42f5cdea51237fcce400c1a9cba33c8c
tsukumijima <tsukumijima@users.noreply.github.com>
2020-08-13 02:25:23 +0900

    MainWindow を開いている時にログが更新されると落ちる不具合を修正

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Collections.Specialized;
@@ -36,7 +36,7 @@ namespace TVRemotePlus_Launcher
             this.Log = (ObservableCollection<string>) Application.Current.Properties["Log"];
             this.Log.CollectionChanged += OnLogChanged;
 
-            // ListBox に追加
+            // ListBox にログを追加
             if (this.Log != null) // null でないなら
             {
                 foreach (var Item in this.Log)
@@ -53,17 +53,20 @@ namespace TVRemotePlus_Launcher
         /// </summary>
         private void OnLogChanged (object sender, NotifyCollectionChangedEventArgs e)
         {
-
             // 新しいログを追加
             var NewLog = e.NewItems;
 
-            // ListBox に追加
             if (NewLog != null) // null でないなら
             {
                 foreach (var Item in NewLog)
                 {
-                    Debug.WriteLine(Item);
-                    //ListBox.Items.Add(Item);
+                    // 別スレッドからコントロールを操作する
+                    // 参考: https://araramistudio.jimdo.com/2017/05/02/c-%E3%81%A7%E5%88%A5%E3%82%B9%E3%83%AC%E3%83%83%E3%83%89%E3%81%8B%E3%82%89%E3%82%B3%E3%83%B3%E3%83%88%E3%83%AD%E3%83%BC%E3%83%AB%E3%82%92%E6%93%8D%E4%BD%9C%E3%81%99%E3%82%8B/
+                    this.Dispatcher.Invoke((Action)(() =>
+                    {
+                        // ListBox に新しいログを追加
+                        ListBox.Items.Add(Item);
+                    }));
                 }
             }
         }
